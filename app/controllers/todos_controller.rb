b30515fd356876021b5ce3892f6ba640cc5e9bb5
Overtaker <angel.overtaker@gmail.com>
2018-01-15 21:07:02 -0500

    era sin la puta S en t@todos.... solo era @todo (en el controler variable de instancia )

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,91 @@
 class TodosController < ApplicationController
 	def index
-		@todos = Todo.all
+		@todo = Todo.all
+	end
+
+	def new
+		@todo = Todo.new
+		render :new
+	end
+
+	def create
+		@todo = Todo.new(todo_params)
+		
+		if @todo.save
+			redirect_to todos_path
+		else
+			render :new
+		end
+	end
+
+	private
+
+	def todo_params
+		params.require(:todo).permit(:description, :completed)
 	end
 end
+
+
+
+
+# class TodosController < ApplicationController
+
+# 	def index
+# 		@todos = Todo.all
+# 	end
+
+# 	def new
+# 		@todo = Todo.new
+# 		render :new
+# 	end
+
+# 	def create
+# 		@todo = Todo.new(todo_params)
+		
+# 		if @todo.save
+# 			redirect_to todos_path
+# 		else
+# 			render :new
+# 		end
+# 	end
+
+# 	def show
+# 		@todo = Todo.find(params[:id])
+# 	end	
+
+# 	def edit
+# 		@todo = Todo.find(params[:id])
+# 	end
+
+# 	def update
+# 		@todo = Todo.find(params[:id])
+# 		if @todo.update(todo_params)
+#       redirect_to todos_path
+#     else
+#       render :edit
+#     end
+# 	end
+
+# 	def destroy
+# 		@todo = Todo.find(params[:id])
+# 		@todo.destroy
+#     redirect_to todos_path
+# 	end
+
+# 	def complete
+# 		@todo = Todo.find(params[:id])
+# 		@todo.completed = true
+# 		@todo.save
+# 		redirect_to todos_path
+# 	end
+
+# 	def list
+# 		@todos = Todo.all
+# 	end
+
+# 	private
+# 	def todo_params
+# 		params.require(:todo).permit(:description, :completed, :id)
+# 	end
+
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,27 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'todos', to: 'todos#index', as: 'todos'
+
+resources :todos #plural
+
+  # get '/todos', to: 'todos#index'
+  # get 'todos/new'
+  # post 'todos', to: 'todos#create'
+
+
+
+  # get 'todos', to: 'todos#index'
+  # get 'todos/new'
+
+  # get 'todos/list'
+  # post 'todos', to: 'todos#create'
+
+  
+  # get 'todos/:id', to: 'todos#show', as: 'todo'
+  # get 'todos/:id/edit', to: 'todos#edit', as: 'editar'
+  # patch 'todos/:id', to: 'todos#update'
+  # delete 'todos/:id', to: 'todos#destroy', as: 'borrar'
+  # get 'complete/:id', to: 'todos#complete', as: 'completar'
+
+
 end
