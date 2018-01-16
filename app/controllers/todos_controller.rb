dded176f31311a7316dcca911cf9e9c75b9a8b38
Overtaker <angel.overtaker@gmail.com>
2018-01-15 23:00:32 -0500

    ok todos los puntos falta el navbar

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -18,10 +18,45 @@ class TodosController < ApplicationController
 		end
 	end
 
+	def show
+		identifica_params
+	end
+
+	def edit
+		identifica_params
+	end
+
+	def update
+		identifica_params
+		if @todo.update(todo_params)
+			@todo.save
+      		redirect_to todos_path
+    	else
+     		render :edit
+    	end
+	end
+
+	def destroy
+		identifica_params
+		@todo.destroy
+    	redirect_to todos_path
+	end
+
+	def completed
+		identifica_params
+		@todo.completed = true
+		@todo.save
+		redirect_to todos_path
+	end
+
 	private
 
 	def todo_params
 		params.require(:todo).permit(:description, :completed)
+	end
+
+	def identifica_params
+		@todo = Todo.find(params[:id])
 	end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,24 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+	root 'todos#index'
+  get '/todos', to: 'todos#index'
+  get 'todos/new'
+  post 'todos', to: 'todos#create'
+  get 'todos/:id', to: 'todos#show', as: 'todo'
+	# Crear la ruta '/todos/:id' que apunte al método todos#show con el verbo get y el prefijo todo.
+  
+  get 'todos/:id/edit', to: 'todos#edit', as:'edit'
+	#Crear la ruta '/todos/:id/edit' que apunte al método todos#edit con el verbo get.
 
-resources :todos #plural
+  patch 'todos/:id', to: 'todos#update'
+	#Crear la ruta '/todos/:id' que apunte al método todos#update con el verbo patch.
 
-  # get '/todos', to: 'todos#index'
-  # get 'todos/new'
-  # post 'todos', to: 'todos#create'
+  delete 'todos/:id', to: 'todos#destroy', as: 'destroy'
+	#Crear la ruta '/todos/:id' que apunte al método todos#destroy con el verbo delete.
 
+  post 'todos/:id/completed', to: 'todos#completed', as: 'completed'
+	#Crear la ruta '/todos/:id/complete' que apunte al método todos#complete con el verbo get
 
 
   # get 'todos', to: 'todos#index'
